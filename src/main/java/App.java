c6b3869c0c2f5b7e083f7fdcb6743e2d6809fe93
gilbertKorir <jerikorir@gmail.com>
2022-04-26 21:59:46 +0300

    initial commits

---
+++ src/main/java/App.java
@@ -0,0 +1,8 @@
+import java.io.Console;
+
+public class App {
+    public static void main(String[] args) {
+        Console myConsole = System.console();
+        System.out.println("Enter the length (or 'height') of your rectangle:");
+    }
+}
---
+++ src/test/java/RectangleTest.java
@@ -0,0 +1,35 @@
+//import org.junit.*;
+import org.junit.jupiter.api.Test;
+
+//import static org.junit.Assert.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RectangleTest {
+
+    @Test
+    public void newRectangle_instantiatesCorrectly() {
+        Rectangle testRectangle = new Rectangle(2, 4);
+        assertEquals(true, testRectangle instanceof Rectangle);
+    }
+
+    @Test
+    public void newRectangle_getsHeight_2() {
+        Rectangle testRectangle = new Rectangle(2, 4);
+        assertEquals(2, testRectangle.getLength());
+    }
+    @Test
+    public void all_returnsAllInstancesOfRectangle_true() {
+        Rectangle firstRectangle = new Rectangle(10, 20);
+        Rectangle secondRectangle = new Rectangle(5, 5);
+        assertTrue(Rectangle.all().contains(firstRectangle));
+        assertTrue(Rectangle.all().contains(secondRectangle));
+    }
+
+    @Test
+    public void getWidth_getsRectangleWidth_4() {
+        Rectangle testRectangle = new Rectangle(2, 4);
+        assertEquals(4, testRectangle.getWidth());
+    }
+}
